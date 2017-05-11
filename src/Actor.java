531128b8b937d48d085f9192cf90cdf1ee6266cc
AllyWang <ally112250@gmail.com>
2017-05-11 13:38:58 -0700

    Added the jar file and added methods in Actor

--- src/Actor.java
+++ src/Actor.java
@@ -1,5 +1,43 @@
 
 public class Actor 
 {
-
+	//private Grid<Actor> grid;
+	private Location location;
+	private int direction;
+	//private Color color;
+	
+	public Actor() {
+		//color = Color.BLACK;
+		//grid = null;
+		location = null;
+	}
+	
+	// getColor()
+	// setColor()
+	
+	public int getDirection() {
+		return direction;
+	}
+	
+	public void setDirection(int newDirection) {
+	
+	}
+	
+	
+	/*
+	public Grid<Actor> getGrid() {
+		return grid;
+	}
+	*/
+	
+	public void act() {
+		
+	}
+	
+	public String toString() {
+		return getClass().getName() + "[location=" + location + ",direction="
+                + direction +  "]";
+	}
+	
+	
 }
