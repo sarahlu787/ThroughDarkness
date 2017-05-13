d55b0a231577c8024c06e5b24e50ce97aeb9c092
AllyWang <ally112250@gmail.com>
2017-05-13 14:14:41 -0700

    added stuff to Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,41 @@
+import java.awt.CardLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Main {
-
-	public Main() {
-		// TODO Auto-generated constructor stub
+public class Main extends JFrame{
+	
+	JPanel cardPanel;
+	
+	public Main(String title) {
+		super(title);
+		setBounds(100, 100, 800, 600);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    cardPanel = new JPanel();
+	    CardLayout cl = new CardLayout();
+	    cardPanel.setLayout(cl);
+	    
+		HomeScreen panel1 = new HomeScreen(this);    
+	    GamePanel panel2 = new GamePanel();
+	    
+	    addKeyListener(panel2.getKeyHandler());
+	
+	    cardPanel.add(panel1,"1");
+	    cardPanel.add(panel2,"2");
+	    
+	    add(cardPanel);
+	
+	    setVisible(true);
 	}
 
-	public void changePanel(String string) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args)
+	{
+		Main w = new Main("ThroughDarkness");
 	}
-
+  
+	public void changePanel(String name) {
+		((CardLayout)cardPanel.getLayout()).show(cardPanel,name);
+		requestFocus();
+	}
 }
